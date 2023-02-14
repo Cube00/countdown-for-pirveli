483167724858dc064d6bcd0b78566bad1f836c17
Cube00 <coller.vato@gmail.com>
2023-02-14 20:29:32 +0400

    countdown!

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,67 @@
-import React from 'react';
-import logo from './logo.svg';
+import {useCountdown} from './utils';
 import './App.css';
 
 function App() {
+  const countDownDate = new Date("Feb 27, 2023 00:00:01").getTime();
+  const [days, hours, minutes, seconds] = useCountdown(countDownDate);
+
+  const startDate = new Date("Feb 1, 2023 00:00:01").getTime();
+  const endDate = new Date("Feb 27, 2023 00:00:01").getTime();
+  const currentDate = new Date().getTime();
+
+  //time percents
+  let x0 = currentDate - startDate;
+  let x1 =  endDate - startDate;
+  let total = (x0 / x1) * 100; 
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="countdown">
+            <div className="backgroundC"></div>
+            <div className="birds">
+                <div className="bird_1">
+                    <img src="https://i.imgur.com/Ry6ziNr.png" />
+                </div>
+                <div className="bird_2">
+                    <img src="https://i.imgur.com/dByVPCO.png" />
+                </div>
+            </div>
+            <div className="content">
+                <div className="header">
+                    <h2>გინდა გაიგო რას გიმზადებთ?</h2>
+                </div>
+                <div className="countDownComponent">
+                    <div className="days countBlock">
+                        <h3>{days}</h3>
+                        <span>დღე</span>
+                    </div>
+                    <div className="to"></div>
+                    <div className="hours countBlock">
+                        <h3>{hours}</h3>
+                        <span>საათი</span>
+                    </div>
+                    <div className="to"></div>
+                    <div className="minutes countBlock">
+                        <h3>{minutes}</h3>
+                        <span>წუთი</span>
+                    </div>
+                    <div className="to"></div>
+                    <div className="seconds countBlock">
+                        <h3>{seconds}</h3>
+                        <span>წამი</span>
+                    </div>
+                </div>
+                <div className="countDownBar">
+                    <div className="countDonwBird" style={{left: `${total - 1}%`}}></div>
+                    <div className="countDownBarProgress" style={{width: `${total}%`}}></div>
+                    <div className="countDownBarBtn" style={{left: `${total}%`}}></div>
+                </div>
+                <div className="countDownInfo">
+                    <span>
+                        დაელოდე 27 თებერვალს და იყავი პირველი!
+                    </span>
+                </div>
+            </div>
+        </div>
   );
 }
 
